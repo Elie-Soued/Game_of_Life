e32e0a25d3c6ba8682f2def0075c2a36d0279fae
Elie <eliegebransoued@gmail.com>
2020-05-30 23:14:51 +0200

    moving circle

--- script.js
+++ script.js
@@ -1,78 +1,90 @@
 var canvas = document.querySelector('canvas');
-canvas.width=480;
-canvas.height=480;
-console.log(canvas);
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
-var c = canvas.getContext('2d');
+
+var c = canvas.getContext ('2d');
 c.strokeStyle='black';
-c.lineWidth=1;
-
-
-
-
-
-
-
-
-for (i=0; i<=innerWidth; i=i+60){
-c.beginPath();
-c.moveTo(i,0);
-c.lineTo(i,innerWidth);
-c.stroke();
-console.log(canvas);
-
-}
-
-for (j=0; j<=innerWidth; j=j+60){
-c.beginPath();
-c.moveTo(0,j);
-c.lineTo(innerWidth,j);
-c.stroke();
-console.log(canvas);
-
-}
-
-
-
-function blink(){
-c.fillRect(240,300,0,0);
-  setTimeout("appear()",500);
-}
-
-
-
-function appear(){
-  c.fillRect(240,300,60,60);
-  setTimeout("blink()",500);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+c.lineWidth=2;
+;
 
 
 /*
-function blink()
-{
-  document.getElementById("logo").style.opacity= 0;
-  setTimeout("appear()",500);
-}
+  for (i=0; i<=innerWidth; i=i+100){
+    for(j=0; j<=innerHeight; j=j+100){
+      c.beginPath();
+      c.fillRect(i,j,50,50)
+      c.stroke();
+      console.log(canvas);
+    }
+      c.beginPath();
+      c.fillRect(i,300,50,50)
+      c.stroke();
+      console.log(canvas);
+      }
+*/
 
-console.log(blink);
+/*
+
+      for (i=0; i<=innerWidth; i=i+50){
+        c.beginPath();
+        c.moveTo(i,0);
+        c.lineTo(i,innerHeight);
+        c.stroke();
+        console.log(canvas);
+        }
+
+        for (j=0; j<=innerHeight; j=j+50){
+          c.beginPath();
+          c.moveTo(0,j);
+          c.lineTo(innerWidth,j);
+          c.stroke();
+          console.log(canvas);
+          }
+
+          */
 
 
-function appear(){
-  document.getElementById("logo").style.opacity= 100;
-  setTimeout("blink()",500);
 
-}
-console.log(appear);
+
+
+
+          var x =Math.random()* innerWidth;
+          var y=Math.random()* innerHeight;
+          var dx= (Math.random()-0.5)*8;
+          var dy=(Math.random()-0.5)*8;
+          var radius=30;
+
+          function animate(){
+            requestAnimationFrame(animate);
+            c.clearRect(0,0,innerWidth,innerHeight);
+
+            c.beginPath();
+            c.arc(x,y,radius,0,Math.PI*2,false);
+            c.strokeStyle='blue';
+            c.stroke();
+
+            if(x+radius>innerWidth||x-radius<0){
+              dx= -dx;
+            }
+            if(y+radius>innerHeight || y-radius<0){
+
+              dy=-dy;
+            }
+
+            x +=dx;
+            y+=dy;
+            }
+
+            animate();
+
+/*
+
+          for (i=0;i<200;i++){
+            var x = Math.random() * window.innerWidth;
+            var y= Math.random() * window.innerHeight;
+            c.beginPath();
+            c.arc(x,y,30,0,Math.PI*2,false);
+            c.fill();
+          }
 */
