862f4e0ee5288ca2637dfdcb685c33b7eb18864b
Elie <eliegebransoued@gmail.com>
2021-06-11 18:39:13 +0200

    moving the eventlistener to a separate file + create the change color function

---
+++ eventlisteners.js
@@ -0,0 +1,43 @@
+//Event Listeners
+//----------------
+
+//Start Game of life
+document.getElementById("run").addEventListener("click", drawgrid);
+
+//Stop Game of life
+document.getElementById("stop").addEventListener("click", () => {
+  location.reload();
+});
+
+//Zoom in
+document.getElementById("ZoomIn").addEventListener("click", () => {
+  interval = interval + 1;
+});
+
+//Zoom out
+document.getElementById("ZoomOut").addEventListener("click", () => {
+  interval = interval - 1;
+});
+
+//Increase Canvas Size
+document.getElementById("IncreaseCanvas").addEventListener("click", () => {
+  canvas.width = canvas.width + 10;
+  canvas.height = canvas.height + 10;
+});
+
+//Decrease Canvas Size
+document.getElementById("DecreaseCanvas").addEventListener("click", () => {
+  canvas.width = canvas.width - 10;
+  canvas.height = canvas.height - 10;
+});
+
+//Change colors
+document.getElementById("Black-White").addEventListener("click", () => {
+  canvas.style.backgroundColor = randomColors();
+  squareColor = randomColors();
+  gridColor = randomColors();
+});
+
+const randomColors = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
