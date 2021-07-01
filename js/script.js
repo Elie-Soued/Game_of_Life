2ac7991f802f2e5ff9349d94e668f3c59499c80a
Elie Soued <eliegebransoued@gmail.com>
2021-07-01 14:53:01 +0200

    creating a renderSquares function to avoid repetition

--- js/script.js
+++ js/script.js
@@ -1,48 +1,30 @@
-//Imports
+//Imports and variable declaration
 import { canvas } from "./canvasClass.js";
 import { Cell } from "./cellClass.js";
-import { drawGrid, make2DArray } from "./constants.js";
+import { drawGrid, make2DArray, countAliveNeighbors } from "./constants.js";
 
-//Variables
 const canvasHTML = document.querySelector("canvas");
 let grid;
-let isRunning = true;
 grid = make2DArray(canvas.cols, canvas.rows);
 
 //Draw an empty grid
 drawGrid();
 
-// fill the grid with cell Objects
+//Fill the grid with cell Objects
 for (let i = 0; i < canvas.cols; i++) {
   for (let j = 0; j < canvas.rows; j++) {
     grid[i][j] = new Cell(i * canvas.interval, j * canvas.interval, canvas);
   }
 }
 
-// Counting the number of alive neighbors of a cell in grid
-function countAliveNeighbors(grid, x, y) {
-  let sum = 0;
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      let col = (x + i + canvas.cols) % canvas.cols;
-      let row = (y + j + canvas.rows) % canvas.rows;
-
-      sum += grid[col][row].state;
-    }
-  }
-  sum = sum - grid[x][y].state;
-  return sum;
-}
-
-// Create a new grid and populate it with rules of Game of Life
-function getnext() {
+//Create a new grid and populate it with rules of Game of Life
+const getnext = () => {
   let next = make2DArray(canvas.cols, canvas.rows);
   for (let i = 0; i < canvas.cols; i++) {
     for (let j = 0; j < canvas.rows; j++) {
       next[i][j] = new Cell(i * canvas.interval, j * canvas.interval, canvas);
     }
   }
-
   for (let i = 0; i < canvas.cols; i++) {
     for (let j = 0; j < canvas.rows; j++) {
       let aliveNeighbors = countAliveNeighbors(grid, i, j);
@@ -58,17 +40,13 @@ function getnext() {
       }
     }
   }
-
   return next;
-}
+};
 
-//Function that display random squares on the grid
-function renderRandomSquares() {
-  drawGrid();
-
+//Function that renders the squares
+const renderSquares = () => {
   for (let i = 0; i < canvas.cols; i++) {
     for (let j = 0; j < canvas.rows; j++) {
-      grid[i][j].state = Math.floor(Math.random() * 2);
       let x = i * canvas.interval;
       let y = j * canvas.interval;
       if (grid[i][j].state == 1) {
@@ -80,45 +58,31 @@ function renderRandomSquares() {
       }
     }
   }
-}
+};
+
+//Function that display random squares on the grid
+const renderRandomSquares = () => {
+  drawGrid();
+  for (let i = 0; i < canvas.cols; i++) {
+    for (let j = 0; j < canvas.rows; j++) {
+      grid[i][j].state = Math.floor(Math.random() * 2);
+    }
+  }
+  renderSquares();
+};
 
 //Function that executes the GOL rules on the squares that are displayed on the grid
-function runGameofLife() {
+const runGameofLife = () => {
   drawGrid();
-
   if (canvas.isRunning) {
-    for (let i = 0; i < canvas.cols; i++) {
-      for (let j = 0; j < canvas.rows; j++) {
-        let x = i * canvas.interval;
-        let y = j * canvas.interval;
-        if (grid[i][j].state == 1) {
-          canvas.c.beginPath();
-          canvas.c.fillStyle = canvas.squareColor;
-          canvas.c.fillRect(x, y, canvas.interval - 1, canvas.interval - 1);
-          canvas.c.strokeStyle = canvas.gridColor;
-          canvas.c.stroke();
-        }
-      }
-    }
+    renderSquares();
     grid = getnext();
     requestAnimationFrame(runGameofLife);
   } else {
-    for (let i = 0; i < canvas.cols; i++) {
-      for (let j = 0; j < canvas.rows; j++) {
-        let x = i * canvas.interval;
-        let y = j * canvas.interval;
-        if (grid[i][j].state == 1) {
-          canvas.c.beginPath();
-          canvas.c.fillStyle = canvas.squareColor;
-          canvas.c.fillRect(x, y, canvas.interval - 1, canvas.interval - 1);
-          canvas.c.strokeStyle = canvas.gridColor;
-          canvas.c.stroke();
-        }
-      }
-    }
+    renderSquares();
     grid = getnext();
   }
-}
+};
 
 //Color cell when user clicks on the canvas
 canvasHTML.addEventListener("click", (e) => {
