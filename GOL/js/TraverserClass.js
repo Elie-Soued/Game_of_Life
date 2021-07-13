44102ab93e59c38a8d932a21d492c87436da5654
Elie Soued <eliegebransoued@gmail.com>
2021-07-13 16:54:38 +0200

    change the second argument of the Traverser´s constructor and naming it executedFunction instead of iterator since the method is called iterate

--- GOL/js/TraverserClass.js
+++ GOL/js/TraverserClass.js
@@ -1,14 +1,14 @@
 class Traverser {
-  constructor(canvas, iterator, grid) {
+  constructor(canvas, executedFunction, grid) {
     this.canvas = canvas;
-    this.iterator = iterator;
+    this.executedFunction = executedFunction;
     this.grid = grid;
   }
 
   iterate() {
     for (let i = 0; i < this.canvas.cols; i++) {
       for (let j = 0; j < this.canvas.rows; j++) {
-        this.iterator(i, j);
+        this.executedFunction(i, j);
       }
     }
   }
