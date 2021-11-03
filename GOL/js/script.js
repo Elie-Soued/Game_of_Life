8baf5ebc4d0db707cae4df96fef64e63ed05330c
Elie Soued <eliegebransoued@gmail.com>
2021-11-04 00:44:23 +0100

    Clean script.js

--- GOL/js/script.js
+++ GOL/js/script.js
@@ -1,83 +1,53 @@
 //Imports and variable declaration
 import { canvas, canvasHTML } from "./canvasClass.js";
-import { Cell } from "./cellClass.js";
-import { drawGrid, make2DArray, countAliveNeighbors } from "./constants.js";
+import {
+  drawGrid,
+  make2DArray,
+  buildNext,
+  fillRandomly,
+  fillSquares,
+} from "./constants.js";
 
-//1-Create a 2D Array called Grid and fill it with empty cells
-let grid = make2DArray(canvas.cols, canvas.rows);
-for (let i = 0; i < canvas.cols; i++) {
-  for (let j = 0; j < canvas.rows; j++) {
-    grid[i][j] = new Cell(i * canvas.interval, j * canvas.interval, canvas);
-  }
-}
+//1-Create a 2D Array called Grid
+let grid = make2DArray(canvas.cols, canvas.rows, canvas);
 
-//2-getnext will create a 2D Array called Next based on Grid and on the Rules of GOL
-const getnext = () => {
-  let next = make2DArray(canvas.cols, canvas.rows);
-  for (let i = 0; i < canvas.cols; i++) {
-    for (let j = 0; j < canvas.rows; j++) {
-      next[i][j] = new Cell(i * canvas.interval, j * canvas.interval, canvas);
-    }
-  }
-
-  //3- Here we check the state of the grid cells + numb of living neighbors
-  for (let i = 0; i < canvas.cols; i++) {
-    for (let j = 0; j < canvas.rows; j++) {
-      let aliveNeighbors = countAliveNeighbors(grid, i, j);
-      //4- these are the GOL Rules
-      if (grid[i][j].state == 0 && aliveNeighbors == 3) {
-        next[i][j].state = 1;
-      } else if (
-        grid[i][j].state == 1 &&
-        (aliveNeighbors < 2 || aliveNeighbors > 3)
-      ) {
-        next[i][j].state = 0;
-      } else {
-        next[i][j].state = grid[i][j].state;
-      }
-    }
-  }
+//2-Create a 2D Array called Next based on Grid and on the Rules of GOL
+const getNext = () => {
+  let next = make2DArray(canvas.cols, canvas.rows, canvas);
+  buildNext(grid, next);
   return next;
 };
 
-//5- renderSquares colors the living cells
+//3- renderSquares colors the living cells
 const renderSquares = () => {
   for (let i = 0; i < canvas.cols; i++) {
     for (let j = 0; j < canvas.rows; j++) {
       let x = i * canvas.interval;
       let y = j * canvas.interval;
       if (grid[i][j].state == 1) {
-        canvas.c.beginPath();
-        canvas.c.fillStyle = canvas.squareColor;
-        canvas.c.fillRect(x, y, canvas.interval - 1, canvas.interval - 1);
-        canvas.c.strokeStyle = canvas.gridColor;
-        canvas.c.stroke();
+        fillSquares(x, y);
       }
     }
   }
 };
 
-//6- renderRandomSquares gives the grid cells a random value of 1 or 0
+//4- renderRandomSquares gives the grid cells a random value of 1 or 0
 const renderRandomSquares = () => {
   drawGrid();
-  for (let i = 0; i < canvas.cols; i++) {
-    for (let j = 0; j < canvas.rows; j++) {
-      grid[i][j].state = Math.floor(Math.random() * 2);
-    }
-  }
+  fillRandomly();
   renderSquares();
 };
 
-//7-runGameOflife runs or stops the GOL Algorithm depending on the value of isRunning
+//5-runGameOflife runs or stops the GOL Algorithm depending on the value of isRunning
 const runGameofLife = () => {
   drawGrid();
   if (canvas.isRunning) {
     renderSquares();
-    grid = getnext();
+    grid = getNext();
     requestAnimationFrame(runGameofLife);
   } else {
     renderSquares();
-    grid = getnext();
+    grid = getNext();
   }
 };
 
